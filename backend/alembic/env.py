6c93884b5960f462f9b7c6fbbcfd852437a2f837
vivekThoke <vivekthoke5@gmail.com>
2026-08-25 09:21:30 +0530

    Backend: configured alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,6 +5,10 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.core.config import get_settings
+from app.models.base import Base
+from app.models import Account, CRMTask, Lead
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,13 +22,18 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+settings = get_settings()
 
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.database_url
+)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
