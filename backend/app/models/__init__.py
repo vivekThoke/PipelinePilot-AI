98efe001ffba228b0120de3346d8fb9971bbb30e
vivekThoke <vivekthoke5@gmail.com>
2026-09-01 23:06:22 +0530

    Backend: updated the package fro the models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,10 +2,14 @@ from app.models.base import Base
 from app.models.account import Account
 from app.models.lead import Lead
 from app.models.task import CRMTask
+from app.models.audit import AuditLog
+from app.models.approval import ApprovalRequest
 
 __all__ = [
     "Account",
     "Base",
     "CRMTask",
     "Lead",
+    "AuditLog",
+    "ApprovalRequest"
 ]
